2bf5ed9dd3457c891cbff4f6bed3aca635e740c7
Devan <huapayadevan@gmail.com>
2015-06-07 17:21:10 -0500

    Added code not functional implementation to
    menu an timr main

--- src/menu.h
+++ src/menu.h
@@ -1,1 +1,17 @@
-#include <pebble.h>
+/*
+ * menu.h
+ *
+ *  Created on: June 7, 2015
+ *      Author: Devan Huapaya
+ */
+
+#ifndef MENU_H_
+#define MENU_H_
+
+void main_menu_init(void);
+void main_menu_close(void);
+void show_menu(void);
+void show_old_watchapp(void);
+void show_old_android(void);
+
+#endif /* MENU_H_ */
--- src/timr.h
+++ src/timr.h
@@ -1,1 +1,18 @@
-#pragma once
+/*
+ * timr.h
+ *
+ *  Created on: June 7, 2015
+ *      Author: Devan Huapaya
+ */
+
+#ifndef TIMR_H_
+#define TIMR_H_
+	
+void setCurWindow(uint8_t newWindow);
+void switchWindow(uint8_t newWindow);
+uint8_t getCurWindow(void);
+const char* config_getFontResource(int id);
+void closeApp(void);
+bool canVibrate(void);
+
+#endif /* TIMR_H_ */
